08faea473aee68c84256a93d74f533d1fc82fab6
DenisImamovicc <Danielarbete@protonmail.com>
2024-03-18 12:02:27 +0100

    add helpful custom desc to rng func.

--- 01-unit-testing/src/randomNum.ts
+++ 01-unit-testing/src/randomNum.ts
@@ -1,1 +1,6 @@
+/**
+ * Returns a random number from 1 to max.
+ * @param max Largest number to possibly return.
+ * @returns
+ */
 export const rng = (max:number = 1) => Math.ceil(Math.random() * max);
